84f235835e0787a896a64353cc599b76a681a073
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-04 13:41:57 -0600

    nav bar and link nav bar configure

--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import Image from 'next/image'
 import { HeroSection } from './components/HeroSection'
+import { Navbar } from './components/Navbar'
 
 export default function Home() {
   return (
     <main 
       className='flex min-h-screen flex-col bg-[#040404]'
     >
-      <div className="container mx-auto px-12 py-4">
+      <Navbar />
+      <div className="container mt-28 mx-auto px-12 py-4">
         <HeroSection />
       </div>
     </main>
